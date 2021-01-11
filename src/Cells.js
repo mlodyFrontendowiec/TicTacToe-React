434ca252365e0a339c8cdc6a5d882376cf5902cc
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-01-11 17:23:56 +0100

    Create initial function and set values

--- src/Cells.js
+++ src/Cells.js
@@ -1,2 +1,20 @@
 import React from "react";
+
 import { ROWS, COLS, CELL } from "./const";
+
+const Cells = () => {
+  const cells = [];
+  for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLS; col++) {
+      cells.push(<div className="cell"></div>);
+    }
+  }
+
+  return (
+    <div style={{ width: COLS * CELL, height: ROWS * CELL }} className="board">
+      {cells}
+    </div>
+  );
+};
+
+export default Cells;
--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import Cells from "./Cells";
+import { START, BODY, FOOD, KEYS } from "./const";
+
+import "./style.css";
 
 const Game = () => {
-  const [board, setBoard] = useState([]);
-  const [snake, setSnake] = useState([]);
+  const [board, setBoard] = useState(null);
+  const [snake, setSnake] = useState(null);
   const [direction, setDirection] = useState(null);
   const [gameover, setGameover] = useState(false);
 
-  return <div>Ok</div>;
+  useEffect(() => {
+    start();
+  }, []);
+  const start = () => {
+    const board = [];
+
+    board[START] = BODY;
+
+    const snake = [START];
+
+    setSnake([snake]);
+    setBoard([board]);
+    setDirection(KEYS.right);
+  };
+
+  return <Cells />;
 };
 
 export default Game;
