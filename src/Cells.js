c99a6df94fe168f9dcac2cb87c440105f3bc6510
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-01-11 16:58:56 +0100

    Create const variables in the project

---
+++ src/Cells.js
@@ -0,0 +1,2 @@
+import React from "react";
+import { ROWS, COLS, CELL } from "./const";
---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Game = () => {
+  const [board, setBoard] = useState([]);
+  const [snake, setSnake] = useState([]);
+  const [direction, setDirection] = useState(null);
+  const [gameover, setGameover] = useState(false);
+
+  return <div>Ok</div>;
+};
+
+export default Game;
