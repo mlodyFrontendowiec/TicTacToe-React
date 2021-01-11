b06efc03da4ec2d94be272fdbb90c77f2594ce6c
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-01-11 18:08:50 +0100

    Create function, which allow move snake

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 import Cells from "./Cells";
-import { START, BODY, FOOD, KEYS } from "./const";
+import { START, BODY, FOOD, KEYS, COLS, ROWS } from "./const";
 
 import "./style.css";
 
 const Game = () => {
-  const [board, setBoard] = useState(null);
   const [snake, setSnake] = useState(null);
+  const [board, setBoard] = useState(null);
   const [direction, setDirection] = useState(null);
   const [gameover, setGameover] = useState(false);
 
@@ -21,9 +21,38 @@ const Game = () => {
 
     const snake = [START];
 
-    setSnake([snake]);
-    setBoard([board]);
+    setSnake(snake);
+    setBoard(board);
     setDirection(KEYS.right);
+  };
+  const frame = () => {
+    const head = getNextIndex(snake[0], direction);
+  };
+  useEffect(() => {
+    console.log(frame());
+  }, [snake, board, direction]);
+
+  const getNextIndex = (head, direction) => {
+    let x = head % COLS;
+    let y = Math.floor(head / COLS);
+
+    switch (direction) {
+      case KEYS.up:
+        y = y <= 0 ? COLS - 1 : y - 1;
+        break;
+      case KEYS.down:
+        y = y >= COLS ? 0 : y + 1;
+        break;
+      case KEYS.left:
+        x = x <= 0 ? COLS - 1 : x - 1;
+        break;
+      case KEYS.right:
+        x = x >= COLS ? 0 : x + 1;
+        break;
+      default:
+        return;
+    }
+    return COLS * y + x;
   };
 
   return <Cells />;
